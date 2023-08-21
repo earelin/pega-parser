f7470e064fd693079ae67da36dc1c59fc4211b87
Xavier Carriba <xavier.carriba@gmail.com>
2023-08-21 14:52:25 +0200

    Basic file reader structure

--- tools/infoelectoral/pkg/file_reader/file_reader.go
+++ tools/infoelectoral/pkg/file_reader/file_reader.go
@@ -1,1 +1,86 @@
 package file_reader
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+type Column struct {
+	name       string
+	position   int
+	length     int
+	columnType string
+}
+
+type FileReader[T any] struct {
+	file     *os.File
+	lineSize int
+	columns  []Column
+}
+
+func (fr FileReader[T]) Read() T {
+	var line T
+	return line
+}
+
+func NewFileReader[T any](file *os.File) (FileReader[T], error) {
+	var structType T
+
+	columns, err := extractColumns[T](structType)
+	if err != nil {
+		return FileReader[T]{}, err
+	}
+
+	return FileReader[T]{
+		file:    file,
+		columns: columns,
+	}, nil
+}
+
+func extractColumns[T any](structType T) ([]Column, error) {
+	var columns []Column
+
+	t := reflect.TypeOf(structType)
+	if t.Kind().String() != "struct" {
+		errorMessage := fmt.Sprintf("not valid type %s", t.Kind().String())
+		return columns, errors.New(errorMessage)
+	}
+
+	e := reflect.ValueOf(&structType).Elem()
+	for i := 0; i < e.NumField(); i++ {
+		f := e.Type().Field(i)
+
+		tags := f.Tag
+		position, err := strconv.Atoi(tags.Get("position"))
+		if err != nil {
+			return []Column{}, err
+		}
+
+		length, err := strconv.Atoi(tags.Get("length"))
+		if err != nil {
+			return []Column{}, err
+		}
+
+		columns = append(columns, Column{
+			columnType: f.Type.Name(),
+			name:       f.Name,
+			position:   position,
+			length:     length,
+		})
+	}
+
+	return columns, nil
+}
+
+func calculateColumnsTotalLength(columns []Column) int {
+	var totalLength int
+
+	for _, column := range columns {
+		totalLength += column.length
+	}
+
+	return totalLength
+}
--- tools/infoelectoral/pkg/file_reader/file_reader_test.go
+++ tools/infoelectoral/pkg/file_reader/file_reader_test.go
@@ -1,7 +1,78 @@
 package file_reader
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestReadFile(t *testing.T) {
 
+}
+
+func Test_extractColumns(t *testing.T) {
+	var tStruct testStruct
+
+	columns, err := extractColumns(tStruct)
+	assert.Nil(t, err, "Should not return error on valid columns")
+	assert.ElementsMatch(t, columnsContent, columns)
+}
+
+func Test_calculateColumnsTotalLength(t *testing.T) {
+	var columns []Column
+
+	assert.Equal(t, 0, calculateColumnsTotalLength(columns))
+
+	columns = []Column{
+		{
+			length: 2,
+		},
+		{
+			length: 2,
+		},
+		{
+			length: 4,
+		},
+	}
+	assert.Equal(t, 8, calculateColumnsTotalLength(columns))
+}
+
+type testStruct struct {
+	year    int    `position:"0" length:"4"`
+	month   int    `position:"4" length:"2"`
+	day     int    `position:"6" length:"2"`
+	weekDay string `position:"8" length:"8"`
+	happy   bool   `position:"16" length:"1"`
+}
+
+var columnsContent = []Column{
+	{
+		name:       "year",
+		position:   0,
+		length:     4,
+		columnType: "int",
+	},
+	{
+		name:       "month",
+		position:   4,
+		length:     2,
+		columnType: "int",
+	},
+	{
+		name:       "day",
+		position:   6,
+		length:     2,
+		columnType: "int",
+	},
+	{
+		name:       "weekDay",
+		position:   8,
+		length:     8,
+		columnType: "string",
+	},
+	{
+		name:       "happy",
+		position:   16,
+		length:     1,
+		columnType: "bool",
+	},
 }
