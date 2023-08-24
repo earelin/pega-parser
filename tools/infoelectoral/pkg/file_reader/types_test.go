dadccb91098cf9c135ba18b56d385d96430eba02
Xavier Carriba <xavier.carriba@gmail.com>
2023-08-24 14:40:32 +0200

    Candidates reader

--- tools/infoelectoral/pkg/file_reader/types_test.go
+++ tools/infoelectoral/pkg/file_reader/types_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestCandidacyRead(t *testing.T) {
+func TestCandidatureRead(t *testing.T) {
 	var f fs.File
 	f, _ = os.Open("testdata/candidatures.DAT")
 	fr, _ := NewFileReader[CandidatureLine](f)
@@ -52,5 +52,83 @@ func TestCandidacyRead(t *testing.T) {
 		c, err := fr.Read()
 		assert.Nil(t, err)
 		assert.Equal(t, candidature, c)
+	}
+}
+
+func TestCandidatesListRead(t *testing.T) {
+	var f fs.File
+	f, _ = os.Open("testdata/candidates.DAT")
+	fr, _ := NewFileReader[CandidatesListLine](f)
+
+	candidates := []CandidatesListLine{
+		{
+			ElectionType:         2,
+			Year:                 2019,
+			Month:                11,
+			Round:                1,
+			ProvinceCode:         7,
+			ElectoralDistrict:    9,
+			MunicipalCode:        999,
+			CandidatureCode:      55,
+			Position:             6,
+			Type:                 "T",
+			Name:                 "Emilio",
+			FirstSurname:         "Hernández",
+			SecondSurname:        "Martín",
+			Genre:                "M",
+			BirthdayDay:          0,
+			BirthdayMonth:        0,
+			BirthdayYear:         0,
+			NationalIdentityCard: "0000000000",
+			Elected:              "N",
+		},
+		{
+			ElectionType:         2,
+			Year:                 2019,
+			Month:                11,
+			Round:                1,
+			ProvinceCode:         15,
+			ElectoralDistrict:    9,
+			MunicipalCode:        999,
+			CandidatureCode:      91,
+			Position:             6,
+			Type:                 "T",
+			Name:                 "Jesús",
+			FirstSurname:         "Fernández",
+			SecondSurname:        "Diez",
+			Genre:                "M",
+			BirthdayDay:          0,
+			BirthdayMonth:        0,
+			BirthdayYear:         0,
+			NationalIdentityCard: "0000000000",
+			Elected:              "N",
+		},
+		{
+			ElectionType:         2,
+			Year:                 2019,
+			Month:                11,
+			Round:                1,
+			ProvinceCode:         41,
+			ElectoralDistrict:    9,
+			MunicipalCode:        999,
+			CandidatureCode:      44,
+			Position:             1,
+			Type:                 "T",
+			Name:                 "Esperanza",
+			FirstSurname:         "Gómez",
+			SecondSurname:        "Corona",
+			Genre:                "F",
+			BirthdayDay:          0,
+			BirthdayMonth:        0,
+			BirthdayYear:         0,
+			NationalIdentityCard: "0000000000",
+			Elected:              "S",
+		},
+	}
+
+	for _, candidate := range candidates {
+		c, err := fr.Read()
+		assert.Nil(t, err)
+		assert.Equal(t, candidate, c)
 	}
 }
