8f76716e4ef238f72afd67206d747e45eae1a84d
Xavier Carriba <xavier.carriba@gmail.com>
2023-09-28 17:22:23 +0200

    Monitoring testing

---
+++ pkg/application/monitoring-controller_test.go
@@ -0,0 +1,21 @@
+package application
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestMonitoringConfig(t *testing.T) {
+	router := gin.Default()
+	MonitoringConfig(router)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/health", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"status":"OK"}`, w.Body.String())
+}
